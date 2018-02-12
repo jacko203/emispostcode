a658b2310b03f14002e376f8b2dad6a9c52ace9f
jacko203 <35066530+jacko203@users.noreply.github.com>
2018-02-12 15:14:51 +0000

    Update emispostcode.h

--- emispostcode.h
+++ emispostcode.h
@@ -8,24 +8,24 @@
 "<?xml version=\"1.0\" encoding=\"utf-8\"?>\
 <enquiryDocument xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xs=\"http://www.w3.org/2001/XMLSchema\" schemaVersion=\"32.0.0\" version=\"1.0.0\" xmlns=\"http://www.e-mis.com/emisopen\">\
 <id>%s</id>\
-<creationTime>2018-01-03T11:29:48.0968914+00:00</creationTime>\
+<creationTime>2018-02-12T15:04:49.4009678+00:00</creationTime>\
 <report>\
-<id>82afdd40-fc68-4143-a440-e557a4e062a7</id>\
+<id>%s</id>\
 <name>Boundary search</name>\
 <description />\
-<creationTime>2018-01-03T11:29:27.4310059+00:00</creationTime>\
-<author><userInRole>f3cfb3cf-ddb8-4fd7-85a5-580a0f920a0c</userInRole></author>\
+<creationTime>2018-02-12T15:04:21.4761241+00:00</creationTime>\
+<author><userInRole>%s</userInRole></author>\
 <populationType>PATIENT</populationType>\
-<folder>53a1b5ad-3fce-43d5-a888-2a6760c6ae88</folder>\
+<folder>%s</folder>\
 <parent parentType=\"ACTIVE\" />\
 <searchDate>BASELINE</searchDate>\
-<population>"
+<population>\
+<criteriaGroup>\
+<definition>\
+<memberOperator>OR</memberOperator>"
 
 #define EPC_LOOP_OUT \
-"<criteriaGroup>\
-<definition>\
-<memberOperator>AND</memberOperator>\
-<criteria>\
+"<criteria>\
 <criterion>\
 <id>%s</id>\
 <table>PATIENTS</table>\
@@ -33,8 +33,7 @@
 <negation>false</negation>\
 <filterAttribute>\
 <columnValue>\
-<id>%s</id>\
-<column>POSTCODE</column>\
+<id>%s</id>\<column>POSTCODE</column>\
 <displayName>Postcode</displayName>\
 <inNotIn>IN</inNotIn>\
 <singleValue>\
@@ -45,42 +44,13 @@
 </columnValue>\
 </filterAttribute>\
 </criterion>\
-</criteria>\
-</definition>\
-<actionIfTrue>REJECT</actionIfTrue>\
-<actionIfFalse>NEXT</actionIfFalse>\
-<id>%s</id>\
-</criteriaGroup>"
+</criteria>"
 
 
 #define EPC_END_OUT "\
-<criteriaGroup>\
-<definition>\
-<memberOperator>AND</memberOperator>\
-<criteria>\
-<criterion>\
-<id>%s</id>\
-<table>PATIENTS</table>\
-<displayName>Patient Details</displayName>\
-<negation>false</negation>\
-<filterAttribute>\
-<columnValue>\
-<id>%s</id>\
-<column>POSTCODE</column>\
-<displayName>Postcode</displayName>\
-<inNotIn>IN</inNotIn>\
-<singleValue>\
-<variable>\
-<value>%s</value>\
-</variable>\
-</singleValue>\
-</columnValue>\
-</filterAttribute>\
-</criterion>\
-</criteria>\
 </definition>\
 <actionIfTrue>REJECT</actionIfTrue>\
-<actionIfFalse>NEXT</actionIfFalse>\
+<actionIfFalse>SELECT</actionIfFalse>\
 <id>%s</id>\
 </criteriaGroup>\
 </population>\
